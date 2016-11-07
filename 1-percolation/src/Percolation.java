fbb8410f64375f336dfeab7e127452136a8a4923
Guillermo Cabrera <guicaro@gmail.com>
2016-11-07 13:16:27 -0600

    First assignment

---
+++ 1-percolation/src/Percolation.java
@@ -0,0 +1,11 @@
+/**
+ * Created by memo on 11/7/16.
+ */
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+
+}
---
+++ 1-percolation/src/PercolationStats.java
@@ -0,0 +1,5 @@
+/**
+ * Created by memo on 11/7/16.
+ */
+public class PercolationStats {
+}
